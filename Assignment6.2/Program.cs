b888a701cfd9bc37aac7821cd38247abe405c6a4
Nicholas Potenza <nicholas.potenza@outlook.com>
2024-09-26 15:49:41 -0500

    Add project files.

---
+++ Assignment6.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> Games = new Stack<string>();
+
+            Games.Push("Call Of Duty");
+            Games.Push("Skyrim");
+            Games.Push("Counter Strike");
+            Games.Push("Rainbow Six");
+            Games.Push("Fallout");
+
+            foreach (var item in Games)
+            {
+                Console.WriteLine(item);
+            }
+
+            Games.Pop();
+            Console.WriteLine();
+            Console.WriteLine("Remaining games after removal:");
+            Console.WriteLine();
+
+            foreach (var item in Games)
+            {
+                Console.WriteLine(item);
+            }
+
+            Games.Pop();
+            Console.WriteLine();
+            Console.WriteLine("Remaining games after removal:");
+            Console.WriteLine();
+
+            foreach (var item in Games)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment6.2/StackArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6._2
+{
+    internal class StackArray
+    {
+        int[] data;
+        int top;
+
+        public StackArray(int size)
+        {
+            data = new int[size];
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == data.Length - 1;
+        }
+
+        public void Push(int value)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is full");
+                return;
+            }
+            else
+            {
+                top++;
+                data[top] = value;
+            }
+        }
+
+        public int Pop()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            else
+            {
+                int value = data[top];
+                top--;
+                return value;
+            }
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i >= 0; i--)
+            {
+                Console.WriteLine(data[i]);
+            }
+            }
+            
+
+        
+    }
+}
